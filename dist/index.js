38b3ab077e10041ab336a939f072ed7136988836
Nik Coughlin <nrkn.com@gmail.com>
2019-02-27 12:48:16 +1300

    Add variable floors and monsters

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,16 @@
 "use strict";
 let s = () => {
-    let sprites = '_`t`_~SA]kkA';
+    let sprites = '_`t`_~SA]kkAAGAAqAA';
     let xor = 65;
     let VIEWSIZE = 9;
     let TILESIZE = 5;
+    let monster = 1;
     let floor = 2;
     let key = (x, y) => x + ',' + y;
     let mapData = {};
     let player = [0, 0];
+    let monsters = [];
+    let monsterChance = 10;
     let draw = () => {
         a.width = a.height = VIEWSIZE * 7 * TILESIZE;
         c.fillStyle = '#000';
@@ -15,12 +18,15 @@ let s = () => {
             for (let viewX = 0; viewX < VIEWSIZE; viewX++) {
                 let mapX = viewX - 4 + player[0];
                 let mapY = viewY - 4 + player[1];
-                let spriteIndex = (viewX === 4 && viewY === 4 ? 0 : -1);
+                let spriteIndex = (viewX === 4 && viewY === 4 ? 0 :
+                    mapData[key(mapX, mapY)] === floor ? floor :
+                        mapData[key(mapX, mapY)] === monster ? monster :
+                            -1);
                 let drawX = viewX * 7 * TILESIZE;
                 let drawY = viewY * 7 * TILESIZE;
                 for (let spriteY = 0; spriteY < 7; spriteY++) {
                     for (let spriteX = 0; spriteX < 7; spriteX++) {
-                        if (spriteIndex >= 0 && (xor ^ sprites.charCodeAt(spriteIndex * 7 + spriteY)) >> spriteX & 1) {
+                        if (spriteIndex > -1 && (xor ^ sprites.charCodeAt(spriteIndex * 7 + spriteY)) >> spriteX & 1) {
                             c.fillRect(spriteX * TILESIZE + drawX, spriteY * TILESIZE + drawY, TILESIZE, TILESIZE);
                         }
                         else if (!mapData[key(mapX, mapY)]) {
@@ -41,13 +47,13 @@ let s = () => {
             let dir = ~~(Math.random() * 4);
             let length = ~~(Math.random() * roomMax * 2) + roomMin * 2;
             let current = junctions[~~(Math.random() * junctions.length)];
-            mapData[key(current[0], current[1])] = floor;
+            mapData[key(current[0], current[1])] = floor + ~~(Math.random() * 2);
             for (let c = 0; c < length; c++) {
                 current = [
                     current[0] + [0, -1, 1, 0][dir],
                     current[1] + [-1, 0, 0, 1][dir]
                 ];
-                mapData[key(current[0], current[1])] = floor;
+                mapData[key(current[0], current[1])] = floor + ~~(Math.random() * 2);
             }
             junctions[junctions.length] = current;
         }
@@ -58,7 +64,15 @@ let s = () => {
             junctions.length--;
             for (let y = 0; y < height; y++) {
                 for (let x = 0; x < width; x++) {
-                    mapData[key(x + current[0] - ~~(width / 2), y + current[1] - ~~(height / 2))] = floor;
+                    mapData[key(x + current[0] - ~~(width / 2), y + current[1] - ~~(height / 2))] = floor + ~~(Math.random() * 2);
+                    if ((x + current[0] - ~~(width / 2)) !== player[0] &&
+                        !~~(Math.random() * monsterChance)) {
+                        monsters[monsters.length] = [
+                            x + current[0] - ~~(width / 2),
+                            y + current[1] - ~~(height / 2)
+                        ];
+                        mapData[key(x + current[0] - ~~(width / 2), y + current[1] - ~~(height / 2))] = monster;
+                    }
                 }
             }
         }
@@ -66,14 +80,26 @@ let s = () => {
     createMap();
     draw();
     b.onkeydown = e => {
-        let y = (e.which === 38 ? -1 :
-            e.which === 40 ? 1 :
-                0);
         let x = (e.which === 37 ? -1 :
             e.which === 39 ? 1 :
                 0);
-        if (mapData[key(player[0] + x, player[1] + y)] === floor) {
+        let y = (e.which === 38 ? -1 :
+            e.which === 40 ? 1 :
+                0);
+        if (mapData[key(player[0] + x, player[1] + y)] === floor ||
+            mapData[key(player[0] + x, player[1] + y)] === floor + 1) {
             player = [player[0] + x, player[1] + y];
+        }
+        for (let i = 0; i < monsters.length; i++) {
+            let dir = ~~(Math.random() * 4);
+            let x = [0, -1, 1, 0][dir];
+            let y = [-1, 0, 0, 1][dir];
+            if (mapData[key(monsters[i][0] + x, monsters[i][1] + y)] === floor ||
+                mapData[key(monsters[i][0] + x, monsters[i][1] + y)] === floor + 1) {
+                mapData[key(monsters[i][0], monsters[i][1])] = floor + ~~(Math.random() * 2);
+                monsters[i] = [monsters[i][0] + x, monsters[i][1] + y];
+                mapData[key(monsters[i][0], monsters[i][1])] = monster;
+            }
         }
         draw();
     };
--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,21 @@ declare const c: CanvasRenderingContext2D
 declare const d: Document
 
 let s = () => {
-  let sprites = '_`t`_~SA]kkA'
+  let sprites = '_`t`_~SA]kkAAGAAqAA'
   let xor = 65
 
   let VIEWSIZE = 9
   let TILESIZE = 5
 
+  let monster = 1
   let floor = 2
 
   let key = ( x, y ) => x + ',' + y
 
-  let mapData = {}
+  let mapData: any = {}
   let player = [ 0, 0 ]
+  let monsters: number[][] = []
+  let monsterChance = 10
 
   let draw = () => {
     a.width = a.height = VIEWSIZE * 7 * TILESIZE
@@ -25,14 +28,17 @@ let s = () => {
         let mapX = viewX - 4 + player[ 0 ]
         let mapY = viewY - 4 + player[ 1 ]
         let spriteIndex = (
-          viewX === 4 && viewY === 4 ? 0 : -1
+          viewX === 4 && viewY === 4 ? 0 :
+          mapData[ key( mapX, mapY ) ] === floor ? floor :
+          mapData[ key( mapX, mapY ) ] === monster ? monster :
+          -1
         )
         let drawX = viewX * 7 * TILESIZE
         let drawY = viewY * 7 * TILESIZE
 
         for ( let spriteY = 0; spriteY < 7; spriteY++ ) {
           for ( let spriteX = 0; spriteX < 7; spriteX++ ) {
-            if ( spriteIndex >= 0 && ( xor ^ sprites.charCodeAt( spriteIndex * 7 + spriteY ) ) >> spriteX & 1 ){
+            if ( spriteIndex > -1 && ( xor ^ sprites.charCodeAt( spriteIndex * 7 + spriteY ) ) >> spriteX & 1 ){
               c.fillRect( spriteX * TILESIZE + drawX, spriteY * TILESIZE + drawY, TILESIZE, TILESIZE )
             } else if ( !mapData[ key( mapX, mapY ) ] ) {
               c.fillRect( spriteX * TILESIZE + drawX, spriteY * TILESIZE + drawY, TILESIZE, TILESIZE )
@@ -55,7 +61,7 @@ let s = () => {
       let length = ~~( Math.random() * roomMax * 2 ) + roomMin * 2
       let current = junctions[ ~~( Math.random() * junctions.length ) ]
 
-      mapData[ key( current[ 0 ], current[ 1 ] ) ] = floor
+      mapData[ key( current[ 0 ], current[ 1 ] ) ] = floor + ~~( Math.random() * 2 )
 
       for ( let c = 0; c < length; c++ ) {
         current = [
@@ -63,7 +69,7 @@ let s = () => {
           current[ 1 ] + [ -1, 0, 0, 1 ][ dir ]
         ]
 
-        mapData[ key( current[ 0 ], current[ 1 ] ) ] = floor
+        mapData[ key( current[ 0 ], current[ 1 ] ) ] = floor + ~~( Math.random() * 2 )
       }
 
       junctions[ junctions.length ] = current
@@ -81,7 +87,21 @@ let s = () => {
           mapData[ key(
             x + current[ 0 ] - ~~( width / 2 ),
             y + current[ 1 ] - ~~( height / 2 )
-          ) ] = floor
+          ) ] = floor + ~~( Math.random() * 2 )
+
+          if (
+            ( x + current[ 0 ] - ~~( width / 2 ) ) !== player[ 0 ] &&
+            !~~( Math.random() * monsterChance )
+          ){
+            monsters[ monsters.length ] = [
+              x + current[ 0 ] - ~~( width / 2 ),
+              y + current[ 1 ] - ~~( height / 2 )
+            ]
+            mapData[ key(
+              x + current[ 0 ] - ~~( width / 2 ),
+              y + current[ 1 ] - ~~( height / 2 )
+            ) ] = monster
+          }
         }
       }
     }
@@ -91,19 +111,38 @@ let s = () => {
   draw()
 
   b.onkeydown = e => {
+    let x = (
+      e.which === 37 ? -1 :
+        e.which === 39 ? 1 :
+          0
+    )
     let y = (
       e.which === 38 ? -1 :
       e.which === 40 ? 1 :
       0
     )
-    let x = (
-      e.which === 37 ? -1 :
-      e.which === 39 ? 1 :
-      0
-    )
 
-    if( mapData[ key( player[ 0 ] + x, player[ 1 ] + y ) ] === floor ){
+    if(
+      mapData[ key( player[ 0 ] + x, player[ 1 ] + y ) ] === floor ||
+      mapData[ key( player[ 0 ] + x, player[ 1 ] + y ) ] === floor + 1
+    ){
       player = [ player[ 0 ] + x, player[ 1 ] + y ]
+    }
+
+    for( let i = 0; i < monsters.length; i++ ){
+      let dir = ~~( Math.random() * 4 )
+
+      let x = [ 0, -1, 1, 0 ][ dir ]
+      let y = [ -1, 0, 0, 1 ][ dir ]
+
+      if (
+        mapData[ key( monsters[ i ][ 0 ] + x, monsters[ i ][ 1 ] + y ) ] === floor ||
+        mapData[ key( monsters[ i ][ 0 ] + x, monsters[ i ][ 1 ] + y ) ] === floor + 1
+      ) {
+        mapData[ key( monsters[ i ][ 0 ], monsters[ i ][ 1 ] ) ] = floor + ~~( Math.random() * 2 )
+        monsters[ i ] = [ monsters[ i ][ 0 ] + x, monsters[ i ][ 1 ] + y ]
+        mapData[ key( monsters[ i ][ 0 ], monsters[ i ][ 1 ] ) ] = monster
+      }
     }
 
     draw()
