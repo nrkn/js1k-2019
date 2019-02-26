b8c0284f6a8e91c3b82c8453e08fdb05d6a80b31
Nik Coughlin <nrkn.com@gmail.com>
2019-02-26 19:43:30 +1300

    Before removing walls etc completely

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,4 @@ declare const a: HTMLCanvasElement;
 declare const b: HTMLBodyElement;
 declare const c: CanvasRenderingContext2D;
 declare const d: Document;
-declare const init: () => void;
+declare let s: () => void;
