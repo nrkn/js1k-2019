6bbe12d05fa2cd4854891267fdd6b3d94006d2fb
Nik Coughlin <nrkn.com@gmail.com>
2019-02-27 17:50:21 +1300

    Potions

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,17 @@
 "use strict";
 let s = () => {
-    let sprites = '_`t`_~SA]kkAAGAAqAA';
+    let sprites = '_@T@_~SA]kkAo@@A@@A_MMS`~_~NRUTT~';
     let xor = 65;
     let VIEWSIZE = 9;
     let TILESIZE = 5;
     let monster = 1;
     let floor = 2;
+    let floor2 = 3;
+    let potion = 4;
+    let monsterSprite = 1;
+    let floorSprite = 2;
+    let potionSprite = 3;
     let key = (x, y) => x + ',' + y;
-    let monsterChance = 1000;
     let mapData;
     let player;
     let monsters;
@@ -18,28 +22,41 @@ let s = () => {
                 let mapX = viewX - 4 + player[0];
                 let mapY = viewY - 4 + player[1];
                 let spriteIndex = -1;
-                let color = 0;
+                let color = 333;
                 if (viewX === 4 && viewY === 4) {
                     spriteIndex = 0;
-                    color = '0fb730'[player[2]];
+                    color = '0fb730'[player[2]] + '31';
                 }
                 else if (mapData[key(mapX, mapY)] === floor) {
-                    spriteIndex = floor;
+                    spriteIndex = floorSprite;
+                    color = 'ddd';
+                }
+                else if (mapData[key(mapX, mapY)] === floor2) {
+                    spriteIndex = floorSprite;
+                    color = 'eee';
+                }
+                else if (mapData[key(mapX, mapY)] === potion) {
+                    spriteIndex = potionSprite;
+                    color = 909;
                 }
                 else if (mapData[key(mapX, mapY)] === monster) {
-                    spriteIndex = monster;
+                    spriteIndex = monsterSprite;
                     for (let i = 0; i < monsters.length; i++) {
-                        if (monsters[i][0] === mapX && monsters[i][1] === mapY) {
-                            color = '0fb730'[monsters[i][2]];
+                        if (monsters[i][0] === mapX
+                            &&
+                                monsters[i][1] === mapY) {
+                            color = '0fb730'[monsters[i][2]] + 13;
                         }
                     }
                 }
                 let drawX = viewX * 7 * TILESIZE;
                 let drawY = viewY * 7 * TILESIZE;
-                c.fillStyle = '#' + color + '00';
+                c.fillStyle = '#' + color;
                 for (let spriteY = 0; spriteY < 7; spriteY++) {
                     for (let spriteX = 0; spriteX < 7; spriteX++) {
-                        if (spriteIndex > -1 && (xor ^ sprites.charCodeAt(spriteIndex * 7 + spriteY)) >> spriteX & 1) {
+                        if (spriteIndex > -1
+                            &&
+                                (xor ^ sprites.charCodeAt(spriteIndex * 7 + spriteY)) >> spriteX & 1) {
                             c.fillRect(spriteX * TILESIZE + drawX, spriteY * TILESIZE + drawY, TILESIZE, TILESIZE);
                         }
                         else if (!mapData[key(mapX, mapY)]) {
@@ -55,21 +72,22 @@ let s = () => {
         player = [0, 0, 5];
         monsters = [];
         let movement = 1000;
+        let monsterChance = movement / 4;
         let current = [0, 0];
         for (let i = 0; i < movement; i++) {
-            for (let wy = -1; wy < 2; wy++) {
-                for (let wx = -1; wx < 2; wx++) {
-                    mapData[key(current[0] + wx, current[1] + wy)] = floor + ~~(Math.random() * 2);
-                    if ((current[0] + wx) !== player[0] &&
-                        !~~(Math.random() * monsterChance)) {
-                        monsters[monsters.length] = [
-                            current[0] + wx,
-                            current[1] + wy,
-                            ~~(Math.random() * 5) + 1
-                        ];
-                        mapData[key(current[0] + wx, current[1] + wy)] = monster;
-                    }
-                }
+            mapData[key(current[0], current[1])] = floor + ~~(Math.random() * 2);
+            if ((current[0]) !== player[0] &&
+                !~~(Math.random() * monsterChance)) {
+                mapData[key(current[0], current[1])] = potion;
+            }
+            else if ((current[0]) !== player[0] &&
+                !~~(Math.random() * monsterChance)) {
+                monsters[monsters.length] = [
+                    current[0],
+                    current[1],
+                    ~~(Math.random() * 5) + 1
+                ];
+                mapData[key(current[0], current[1])] = monster;
             }
             let dir = ~~(Math.random() * 4);
             current = [
@@ -77,64 +95,6 @@ let s = () => {
                 current[1] + [-1, 0, 0, 1][dir]
             ];
         }
-        /*
-        let roomCount = 20
-        let junctionCount = 40
-        let roomMin = 4
-        let roomMax = 12
-        let junctions = [ [ 0, 0 ] ]
-    
-        while ( junctionCount-- ) {
-          let dir = ~~( Math.random() * 4 )
-          let length = ~~( Math.random() * roomMax * 2 ) + roomMin * 2
-          let current = junctions[ ~~( Math.random() * junctions.length ) ]
-    
-          mapData[ key( current[ 0 ], current[ 1 ] ) ] = floor + ~~( Math.random() * 2 )
-    
-          for ( let c = 0; c < length; c++ ) {
-            current = [
-              current[ 0 ] + [ 0, -1, 1, 0 ][ dir ],
-              current[ 1 ] + [ -1, 0, 0, 1 ][ dir ]
-            ]
-    
-            mapData[ key( current[ 0 ], current[ 1 ] ) ] = floor + ~~( Math.random() * 2 )
-          }
-    
-          junctions[ junctions.length ] = current
-        }
-    
-        while ( roomCount-- ) {
-          let width = ~~( Math.random() * roomMax ) + roomMin
-          let height = ~~( Math.random() * roomMax ) + roomMin
-          let current = junctions[ junctions.length - 1 ]
-    
-          junctions.length--
-    
-          for ( let y = 0; y < height; y++ ) {
-            for ( let x = 0; x < width; x++ ) {
-              mapData[ key(
-                x + current[ 0 ] - ~~( width / 2 ),
-                y + current[ 1 ] - ~~( height / 2 )
-              ) ] = floor + ~~( Math.random() * 2 )
-    
-              if (
-                ( x + current[ 0 ] - ~~( width / 2 ) ) !== player[ 0 ] &&
-                !~~( Math.random() * monsterChance )
-              ){
-                monsters[ monsters.length ] = [
-                  x + current[ 0 ] - ~~( width / 2 ),
-                  y + current[ 1 ] - ~~( height / 2 ),
-                  5
-                ]
-                mapData[ key(
-                  x + current[ 0 ] - ~~( width / 2 ),
-                  y + current[ 1 ] - ~~( height / 2 )
-                ) ] = monster
-              }
-            }
-          }
-        }
-        */
     };
     createMap();
     draw();
@@ -145,9 +105,14 @@ let s = () => {
         let y = (e.which === 38 ? -1 :
             e.which === 40 ? 1 :
                 0);
-        if (mapData[key(player[0] + x, player[1] + y)] === floor ||
-            mapData[key(player[0] + x, player[1] + y)] === floor + 1) {
-            player = [player[0] + x, player[1] + y, player[2]];
+        if (mapData[key(player[0] + x, player[1] + y)] === floor
+            ||
+                mapData[key(player[0] + x, player[1] + y)] === floor2) {
+            player = [
+                player[0] + x,
+                player[1] + y,
+                player[2]
+            ];
         }
         else if (mapData[key(player[0] + x, player[1] + y)] === monster) {
             for (let i = 0; i < monsters.length; i++) {
@@ -162,8 +127,15 @@ let s = () => {
                 }
             }
         }
+        else if (mapData[key(player[0] + x, player[1] + y)] === potion) {
+            mapData[key(player[0] + x, player[1] + y)] = floor + ~~(Math.random() * 2);
+            if (player[2] < 5)
+                player[2]++;
+        }
         for (let i = 0; i < monsters.length; i++) {
-            if ((x || y) && monsters[i][2]) {
+            if ((x || y)
+                &&
+                    monsters[i][2]) {
                 let action = ~~(Math.random() * 4);
                 let dir = ~~(Math.random() * 4);
                 if (action < 2) {
@@ -178,16 +150,25 @@ let s = () => {
                     x = 0;
                     y = player[1] < monsters[i][1] ? -1 : 1;
                 }
-                if (monsters[i][0] + x === player[0] && monsters[i][1] + y === player[1]) {
-                    player[2]--;
-                    if (!player[2]) {
-                        createMap();
+                if (monsters[i][0] + x === player[0]
+                    &&
+                        monsters[i][1] + y === player[1]) {
+                    if (~~(Math.random() * 2)) {
+                        player[2]--;
+                        if (!player[2]) {
+                            createMap();
+                        }
                     }
                 }
-                else if (mapData[key(monsters[i][0] + x, monsters[i][1] + y)] === floor ||
-                    mapData[key(monsters[i][0] + x, monsters[i][1] + y)] === floor + 1) {
+                else if (mapData[key(monsters[i][0] + x, monsters[i][1] + y)] === floor
+                    ||
+                        mapData[key(monsters[i][0] + x, monsters[i][1] + y)] === floor2) {
                     mapData[key(monsters[i][0], monsters[i][1])] = floor + ~~(Math.random() * 2);
-                    monsters[i] = [monsters[i][0] + x, monsters[i][1] + y, monsters[i][2]];
+                    monsters[i] = [
+                        monsters[i][0] + x,
+                        monsters[i][1] + y,
+                        monsters[i][2]
+                    ];
                     mapData[key(monsters[i][0], monsters[i][1])] = monster;
                 }
             }
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ declare const c: CanvasRenderingContext2D
 declare const d: Document
 
 let s = () => {
-  let sprites = '_`t`_~SA]kkAAGAAqAA'
+  let sprites = '_@T@_~SA]kkAo@@A@@A_MMS`~_~NRUTT~'
   let xor = 65
 
   let VIEWSIZE = 9
@@ -12,10 +12,13 @@ let s = () => {
 
   let monster = 1
   let floor = 2
+  let floor2 = 3
+  let potion = 4
+  let monsterSprite = 1
+  let floorSprite = 2
+  let potionSprite = 3
 
   let key = ( x, y ) => x + ',' + y
-
-  let monsterChance = 1000
 
   let mapData: any
   let player: number[]
@@ -28,18 +31,49 @@ let s = () => {
         let mapX = viewX - 4 + player[ 0 ]
         let mapY = viewY - 4 + player[ 1 ]
         let spriteIndex = -1
-        let color: string | number = 0
+        let color: string | number = 333
 
         if( viewX === 4 && viewY === 4 ){
           spriteIndex = 0
-          color = '0fb730'[ player[ 2 ] ]
-        } else if ( mapData[ key( mapX, mapY ) ] === floor ){
-          spriteIndex = floor
-        } else if ( mapData[ key( mapX, mapY ) ] === monster ){
-          spriteIndex = monster
+          color = '0fb730'[ player[ 2 ] ] + '31'
+        } else if (
+          mapData[ key(
+            mapX,
+            mapY
+          ) ] === floor
+        ){
+          spriteIndex = floorSprite
+          color = 'ddd'
+        } else if(
+          mapData[ key(
+            mapX,
+            mapY
+          ) ] === floor2
+        ){
+          spriteIndex = floorSprite
+          color = 'eee'
+        } else if (
+          mapData[ key(
+            mapX,
+            mapY
+          ) ] === potion
+        ) {
+          spriteIndex = potionSprite
+          color = 909
+        } else if (
+          mapData[ key(
+            mapX,
+            mapY
+          ) ] === monster
+        ){
+          spriteIndex = monsterSprite
           for ( let i = 0; i < monsters.length; i++ ) {
-            if ( monsters[ i ][ 0 ] === mapX && monsters[ i ][ 1 ] === mapY ) {
-              color = '0fb730'[ monsters[ i ][ 2 ] ]
+            if (
+              monsters[ i ][ 0 ] === mapX
+              &&
+              monsters[ i ][ 1 ] === mapY
+            ) {
+              color = '0fb730'[ monsters[ i ][ 2 ] ] + 13
             }
           }
         }
@@ -47,14 +81,29 @@ let s = () => {
         let drawX = viewX * 7 * TILESIZE
         let drawY = viewY * 7 * TILESIZE
 
-        c.fillStyle = '#' + color + '00'
+        c.fillStyle = '#' + color
 
         for ( let spriteY = 0; spriteY < 7; spriteY++ ) {
           for ( let spriteX = 0; spriteX < 7; spriteX++ ) {
-            if ( spriteIndex > -1 && ( xor ^ sprites.charCodeAt( spriteIndex * 7 + spriteY ) ) >> spriteX & 1 ){
-              c.fillRect( spriteX * TILESIZE + drawX, spriteY * TILESIZE + drawY, TILESIZE, TILESIZE )
-            } else if ( !mapData[ key( mapX, mapY ) ] ) {
-              c.fillRect( spriteX * TILESIZE + drawX, spriteY * TILESIZE + drawY, TILESIZE, TILESIZE )
+            if (
+              spriteIndex > -1
+              &&
+              ( xor ^ sprites.charCodeAt( spriteIndex * 7 + spriteY ) ) >> spriteX & 1
+            ){
+              c.fillRect(
+                spriteX * TILESIZE + drawX, spriteY * TILESIZE + drawY,
+                TILESIZE, TILESIZE
+              )
+            } else if (
+              !mapData[ key(
+                mapX,
+                mapY
+              ) ]
+            ) {
+              c.fillRect(
+                spriteX * TILESIZE + drawX, spriteY * TILESIZE + drawY,
+                TILESIZE, TILESIZE
+              )
             }
           }
         }
@@ -68,28 +117,37 @@ let s = () => {
     monsters = []
 
     let movement = 1000
+    let monsterChance = movement / 4
     let current = [ 0, 0 ]
 
     for( let i = 0; i < movement; i++ ){
-      for( let wy = -1; wy < 2; wy++ ){
-        for( let wx = -1; wx < 2; wx++ ){
-          mapData[ key( current[ 0 ] + wx, current[ 1 ] + wy ) ] = floor + ~~( Math.random() * 2 )
+      mapData[ key(
+        current[ 0 ],
+        current[ 1 ]
+      ) ] = floor + ~~( Math.random() * 2 )
 
-          if (
-            ( current[ 0 ] + wx ) !== player[ 0 ] &&
-            !~~( Math.random() * monsterChance )
-          ) {
-            monsters[ monsters.length ] = [
-              current[ 0 ] + wx,
-              current[ 1 ] + wy,
-              ~~( Math.random() * 5 ) + 1
-            ]
-            mapData[ key(
-              current[ 0 ] + wx,
-              current[ 1 ] + wy
-            ) ] = monster
-          }
-        }
+      if (
+        ( current[ 0 ] ) !== player[ 0 ] &&
+        !~~( Math.random() * monsterChance )
+      ) {
+        mapData[ key(
+          current[ 0 ],
+          current[ 1 ]
+        ) ] = potion
+      }
+      else if (
+        ( current[ 0 ] ) !== player[ 0 ] &&
+        !~~( Math.random() * monsterChance )
+      ) {
+        monsters[ monsters.length ] = [
+          current[ 0 ],
+          current[ 1 ],
+          ~~( Math.random() * 5 ) + 1
+        ]
+        mapData[ key(
+          current[ 0 ],
+          current[ 1 ]
+        ) ] = monster
       }
 
       let dir = ~~( Math.random() * 4 )
@@ -99,65 +157,6 @@ let s = () => {
         current[ 1 ] + [ -1, 0, 0, 1 ][ dir ]
       ]
     }
-
-    /*
-    let roomCount = 20
-    let junctionCount = 40
-    let roomMin = 4
-    let roomMax = 12
-    let junctions = [ [ 0, 0 ] ]
-
-    while ( junctionCount-- ) {
-      let dir = ~~( Math.random() * 4 )
-      let length = ~~( Math.random() * roomMax * 2 ) + roomMin * 2
-      let current = junctions[ ~~( Math.random() * junctions.length ) ]
-
-      mapData[ key( current[ 0 ], current[ 1 ] ) ] = floor + ~~( Math.random() * 2 )
-
-      for ( let c = 0; c < length; c++ ) {
-        current = [
-          current[ 0 ] + [ 0, -1, 1, 0 ][ dir ],
-          current[ 1 ] + [ -1, 0, 0, 1 ][ dir ]
-        ]
-
-        mapData[ key( current[ 0 ], current[ 1 ] ) ] = floor + ~~( Math.random() * 2 )
-      }
-
-      junctions[ junctions.length ] = current
-    }
-
-    while ( roomCount-- ) {
-      let width = ~~( Math.random() * roomMax ) + roomMin
-      let height = ~~( Math.random() * roomMax ) + roomMin
-      let current = junctions[ junctions.length - 1 ]
-
-      junctions.length--
-
-      for ( let y = 0; y < height; y++ ) {
-        for ( let x = 0; x < width; x++ ) {
-          mapData[ key(
-            x + current[ 0 ] - ~~( width / 2 ),
-            y + current[ 1 ] - ~~( height / 2 )
-          ) ] = floor + ~~( Math.random() * 2 )
-
-          if (
-            ( x + current[ 0 ] - ~~( width / 2 ) ) !== player[ 0 ] &&
-            !~~( Math.random() * monsterChance )
-          ){
-            monsters[ monsters.length ] = [
-              x + current[ 0 ] - ~~( width / 2 ),
-              y + current[ 1 ] - ~~( height / 2 ),
-              5
-            ]
-            mapData[ key(
-              x + current[ 0 ] - ~~( width / 2 ),
-              y + current[ 1 ] - ~~( height / 2 )
-            ) ] = monster
-          }
-        }
-      }
-    }
-    */
   }
 
   createMap()
@@ -176,12 +175,27 @@ let s = () => {
     )
 
     if(
-      mapData[ key( player[ 0 ] + x, player[ 1 ] + y ) ] === floor ||
-      mapData[ key( player[ 0 ] + x, player[ 1 ] + y ) ] === floor + 1
+      mapData[ key(
+        player[ 0 ] + x,
+        player[ 1 ] + y
+      ) ] === floor
+      ||
+      mapData[ key(
+        player[ 0 ] + x,
+        player[ 1 ] + y )
+      ] === floor2
     ){
-      player = [ player[ 0 ] + x, player[ 1 ] + y, player[ 2 ] ]
-    } else if(
-      mapData[ key( player[ 0 ] + x, player[ 1 ] + y ) ] === monster
+      player = [
+        player[ 0 ] + x,
+        player[ 1 ] + y,
+        player[ 2 ]
+      ]
+    }
+    else if(
+      mapData[ key(
+        player[ 0 ] + x,
+        player[ 1 ] + y
+      ) ] === monster
     ){
       for ( let i = 0; i < monsters.length; i++ ) {
         if (
@@ -192,15 +206,36 @@ let s = () => {
             monsters[ i ][ 2 ]--
 
             if ( !monsters[ i ][ 2 ] ) {
-              mapData[ key( player[ 0 ] + x, player[ 1 ] + y ) ] = floor + ~~( Math.random() * 2 )
+              mapData[ key(
+                player[ 0 ] + x,
+                player[ 1 ] + y
+              ) ] = floor + ~~( Math.random() * 2 )
             }
           }
         }
       }
     }
+    else if (
+      mapData[ key(
+        player[ 0 ] + x,
+        player[ 1 ] + y
+      ) ] === potion
+    ) {
+      mapData[ key(
+        player[ 0 ] + x,
+        player[ 1 ] + y
+      ) ] = floor + ~~( Math.random() * 2 )
+
+      if( player[ 2 ] < 5 )
+        player[ 2 ]++
+    }
 
     for( let i = 0; i < monsters.length; i++ ){
-      if ( ( x || y ) && monsters[ i ][ 2 ] ){
+      if (
+        ( x || y )
+        &&
+        monsters[ i ][ 2 ]
+      ){
         let action = ~~( Math.random() * 4 )
         let dir = ~~( Math.random() * 4 )
 
@@ -215,7 +250,11 @@ let s = () => {
           y = player[ 1 ] < monsters[ i ][ 1 ] ? -1 : 1
         }
 
-        if ( monsters[ i ][ 0 ] + x === player[ 0 ] && monsters[ i ][ 1 ] + y === player[ 1 ] ) {
+        if (
+          monsters[ i ][ 0 ] + x === player[ 0 ]
+          &&
+          monsters[ i ][ 1 ] + y === player[ 1 ]
+        ) {
           if( ~~( Math.random() * 2 ) ){
             player[ 2 ]--
 
@@ -224,12 +263,31 @@ let s = () => {
             }
           }
         } else if (
-          mapData[ key( monsters[ i ][ 0 ] + x, monsters[ i ][ 1 ] + y ) ] === floor ||
-          mapData[ key( monsters[ i ][ 0 ] + x, monsters[ i ][ 1 ] + y ) ] === floor + 1
+          mapData[ key(
+            monsters[ i ][ 0 ] + x,
+            monsters[ i ][ 1 ] + y
+          ) ] === floor
+          ||
+          mapData[ key(
+            monsters[ i ][ 0 ] + x,
+            monsters[ i ][ 1 ] + y
+          ) ] === floor2
         ) {
-          mapData[ key( monsters[ i ][ 0 ], monsters[ i ][ 1 ] ) ] = floor + ~~( Math.random() * 2 )
-          monsters[ i ] = [ monsters[ i ][ 0 ] + x, monsters[ i ][ 1 ] + y, monsters[ i ][ 2 ] ]
-          mapData[ key( monsters[ i ][ 0 ], monsters[ i ][ 1 ] ) ] = monster
+          mapData[ key(
+            monsters[ i ][ 0 ],
+            monsters[ i ][ 1 ]
+          ) ] = floor + ~~( Math.random() * 2 )
+
+          monsters[ i ] = [
+            monsters[ i ][ 0 ] + x,
+            monsters[ i ][ 1 ] + y,
+            monsters[ i ][ 2 ]
+          ]
+
+          mapData[ key(
+            monsters[ i ][ 0 ],
+            monsters[ i ][ 1 ]
+          ) ] = monster
         }
       }
     }
